2148795af5a0fb8b2612972fe4f9545f571cd50e
manofmoutain <m97532007@gmail.com>
2021-02-04 10:05:52 +0800

    03

---
+++ Assets/Resource/03.Heimlich maneuver/Scripts/HamlickPatient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+namespace Heimlich_maneuver.Patient
+{
+    public class HamlickPatient : MonoBehaviour
+    {
+        [SerializeField] private GameObject player;
+        [SerializeField] private GameObject interactPint;
+
+
+        private void Start()
+        {
+            player = FindObjectOfType<Player>().gameObject;
+        }
+
+        private void Update()
+        {
+            interactPint.SetActive(player.transform.position.z > transform.position.z);
+        }
+    }
+}
