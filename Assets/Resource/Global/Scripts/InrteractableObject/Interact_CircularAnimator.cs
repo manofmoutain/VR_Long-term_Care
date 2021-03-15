68ced166fef09b0129978f542b3607d88777a534
manofmoutain <m97532007@gmail.com>
2021-03-15 13:02:36 +0800

    增加：圓形驅動動畫腳本

---
+++ Assets/Resource/Global/Scripts/InrteractableObject/Interact_CircularAnimator.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace InteractableObject
+{
+    public class Interact_CircularAnimator : MonoBehaviour
+    {
+        public GameObject circularDriveGameObject;
+        [SerializeField] private Animator animator;
+
+        public float currentCircularMapping = float.NaN;
+        [SerializeField] private int framesUnchanged = 0;
+
+
+        private void Awake()
+        {
+            if (animator == null)
+            {
+                animator.GetComponent<Animator>();
+            }
+
+            animator.speed = 0.0f;
+        }
+
+
+        private void Update()
+        {
+#if UNITY_ANDROID && !UNITY_EDITOR
+ // Oculus Quest代碼
+
+#else
+// SteamVR代碼
+            if (currentCircularMapping != circularDriveGameObject.GetComponent<Interact_CircularDrive>().linearMapping.value)
+            {
+                currentCircularMapping = circularDriveGameObject.GetComponent<Interact_CircularDrive>().linearMapping.value;
+                animator.enabled = true;
+                animator.Play(0, 0, currentCircularMapping);
+                framesUnchanged = 0;
+            }
+            else
+            {
+                framesUnchanged++;
+                if (framesUnchanged > 2)
+                {
+                    animator.enabled = false;
+                }
+            }
+#endif
+        }
+    }
+}
+
