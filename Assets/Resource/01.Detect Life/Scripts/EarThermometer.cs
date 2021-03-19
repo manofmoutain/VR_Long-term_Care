04648818b7d88ef92ff04ed5f1398e63334d6858
manofmoutain <m97532007@gmail.com>
2021-03-19 16:50:57 +0800

    增加：溫度計-抓取，貼合，抓取時開關電源

---
+++ Assets/Resource/01.Detect Life/Scripts/EarThermometer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+namespace DetectLife
+{
+    public class EarThermometer : MonoBehaviour
+    {
+        [SerializeField] private bool isOn;
+        [SerializeField] private GameObject pointLight;
+        [SerializeField] private Material offMaterial;
+        [SerializeField] private Material onMaterial;
+        [SerializeField] private MeshRenderer meshRenderer;
+
+        private void Start()
+        {
+            pointLight.SetActive(false);
+        }
+
+
+        void HandAttachedUpdate(Hand hand)
+        {
+            GrabTypes grabTypes = hand.GetGrabStarting();
+
+            if (grabTypes == GrabTypes.Pinch)
+            {
+                print("123");
+                if (isOn)
+                {
+                    isOn = false;
+                    meshRenderer.material = offMaterial;
+                    CancelInvoke(nameof(Switch));
+                }
+                else
+                {
+                    isOn = true;
+                    meshRenderer.material = onMaterial;
+                    InvokeRepeating(nameof(Switch), 1, 0.5f);
+                }
+            }
+        }
+
+        public void Switch()
+        {
+            pointLight.SetActive(!pointLight.activeSelf);
+        }
+    }
+}
