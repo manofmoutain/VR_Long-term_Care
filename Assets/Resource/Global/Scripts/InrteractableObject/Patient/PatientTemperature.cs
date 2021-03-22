0448287ea63b77657b7ed2c906d16b748b7f6ef1
manofmoutain <m97532007@gmail.com>
2021-03-22 13:42:17 +0800

    增加：血壓、體溫腳本

---
+++ Assets/Resource/Global/Scripts/InrteractableObject/Patient/PatientTemperature.cs
@@ -0,0 +1,25 @@
+﻿using DetectLife;
+using UnityEngine;
+namespace Global.Pateint
+{
+    public class PatientTemperature : MonoBehaviour
+    {
+        [SerializeField] private float[] temperatures;
+        [SerializeField] private float temperature;
+
+        public float Temperature => temperature;
+
+
+        private void Start()
+        {
+            int randomTemperature = Random.Range(0, temperatures.Length);
+            temperature = temperatures[randomTemperature];
+        }
+
+        public void ShowTemperature(EarThermometer earThermometer)
+        {
+            earThermometer.ShowTemperature(Temperature);
+        }
+    }
+}
+
