888847b009417f6a44a4b000a5fe2f073d1ad2f9
manofmoutain <m97532007@gmail.com>
2021-03-24 13:26:00 +0800

    增加：水龍頭轉開腳本與打開水流特效

---
+++ Assets/Resource/04.Prepare Meal/Scripts/FaucetCreateFlow.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+namespace PrepareMeal
+{
+    public class FaucetCreateFlow : MonoBehaviour
+    {
+        [SerializeField] private ParticleSystem vfx;
+        [SerializeField] private bool isFlowing;
+        [SerializeField] private float timer;
+
+        private void Start()
+        {
+            vfx.gameObject.SetActive(false);
+            isFlowing = false;
+        }
+
+        private void Update()
+        {
+            if (isFlowing)
+            {
+                timer += Time.deltaTime;
+            }
+            else
+            {
+                timer = 0;
+            }
+        }
+
+        public void SwitchFlow(bool on)
+        {
+            isFlowing = on;
+            vfx.gameObject.SetActive(on);
+        }
+    }
+}
+
