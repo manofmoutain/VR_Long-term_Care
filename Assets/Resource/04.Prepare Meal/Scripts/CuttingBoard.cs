7d5bdd22ed12bc6640e7597b080f6973a33032fc
manofmoutain <m97532007@gmail.com>
2021-03-24 16:07:16 +0800

    增加：砧板抓取與放置以及砧板腳本

---
+++ Assets/Resource/04.Prepare Meal/Scripts/CuttingBoard.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PrepareMeal
+{
+    public class CuttingBoard : MonoBehaviour
+    {
+        public bool isCleaned;
+
+
+
+        public void MakeCLeaned(bool clean)
+        {
+            isCleaned = clean;
+        }
+
+        public void SetToRightPosition()
+        {
+            if (isCleaned)
+            {
+
+            }
+        }
+    }
+}
+
