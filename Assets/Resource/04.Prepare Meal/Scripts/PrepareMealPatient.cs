4dc3a894f9c0b7aa1cf0c2415c2158f4828f33b2
manofmoutain <m97532007@gmail.com>
2021-03-24 13:26:51 +0800

    增加：備餐案主腳本_空的

---
+++ Assets/Resource/04.Prepare Meal/Scripts/PrepareMealPatient.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PrepareMeal
+{
+    public class PrepareMealPatient : MonoBehaviour
+    {
+
+
+
+
+        public void WashHand()
+        {
+
+        }
+    }
+}
+
