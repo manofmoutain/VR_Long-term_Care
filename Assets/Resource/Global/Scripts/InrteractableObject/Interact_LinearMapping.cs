8a852d3e57ad8db8b71da53a58da7f31031f0b40
1234 <132>
2021-05-27 08:59:12 +0800

    新增：Interact_LinearMapping  獨立腳本

---
+++ Assets/Resource/Global/Scripts/InrteractableObject/Interact_LinearMapping.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace InteractableObject
+{
+    public class Interact_LinearMapping : MonoBehaviour
+    {
+        [Tooltip("推動的值(0-1)") ]public float value;
+    }
+}
+
