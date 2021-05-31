0604ec149c24789197e859f7b9b8f462e3776697
1234 <132>
2021-05-31 16:29:25 +0800

    增加腳本：用事件驅動動畫

---
+++ Assets/Resource/Global/Scripts/InrteractableObject/NoUsingValve/AnimatorTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace InteractableObject
+{
+    public class AnimatorTrigger : MonoBehaviour
+    {
+        [SerializeField] Animator animator;
+
+        private void Start()
+        {
+            animator = GetComponent<Animator>();
+        }
+
+        public void SetAnimatorTrigger(string parameter)
+        {
+            animator.SetTrigger(parameter);
+        }
+
+        public void SetAnimatorBool(string parameter)
+        {
+            animator.SetBool(parameter,!animator.GetBool(parameter));
+        }
+    }
+}
+
