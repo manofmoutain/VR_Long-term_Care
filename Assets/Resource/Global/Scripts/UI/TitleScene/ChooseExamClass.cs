d1aca2c0327c86a52d2bbc9eac40acd6e5682130
manofmoutain <m97532007@gmail.com>
2021-06-17 15:04:43 +0800

    手錶時間的腳本、ChooseExamClass腳本增加是否要讀取Excel檔的判斷

--- Assets/Resource/Global/Scripts/UI/TitleScene/ChooseExamClass.cs
+++ Assets/Resource/Global/Scripts/UI/TitleScene/ChooseExamClass.cs
@@ -8,22 +8,27 @@ namespace TitleUIScripts
 {
     public class ChooseExamClass : MonoBehaviour
     {
+        [SerializeField] private bool isLoadExel;
         [SerializeField] private List<Button> examBtns;
         [SerializeField] private List<ExaminClass> examClass;
         [SerializeField] private Button backBtn;
 
         private void Start()
         {
-            // for (int i = 0; i < examClass.Count; i++)
-            // {
-            //     ScoreManager.Instance.Initialize(examClass[i].fileName, examClass[i].operateCount , examClass[i].limitTime);
-            //     examClass[i].lesson = ScoreManager.Instance.GetLesson();
-            //     examClass[i].operateTopics.Clear();
-            //     for (int j = 0; j < examClass[i].operateCount; j++)
-            //     {
-            //         examClass[i].operateTopics.Add(ScoreManager.Instance.GetOperateTopic(j));
-            //     }
-            // }
+            if (isLoadExel)
+            {
+                for (int i = 0; i < examClass.Count; i++)
+                {
+                    ScoreManager.Instance.Initialize(examClass[i].fileName, examClass[i].operateCount , examClass[i].limitTime);
+                    examClass[i].lesson = ScoreManager.Instance.GetLesson();
+                    examClass[i].operateTopics.Clear();
+                    for (int j = 0; j < examClass[i].operateCount; j++)
+                    {
+                        examClass[i].operateTopics.Add(ScoreManager.Instance.GetOperateTopic(j));
+                    }
+                }
+            }
+
 
 
             backBtn.onClick.AddListener(delegate
@@ -35,8 +40,15 @@ namespace TitleUIScripts
 
         public void EnterExam(int index)
         {
-            // ScoreManager.Instance.Initialize(examClass[index].fileName, examClass[index].operateCount , examClass[index].limitTime);
-            ScoreManager.Instance.ReadExamData(index);
+            if (isLoadExel)
+            {
+                ScoreManager.Instance.Initialize(examClass[index].fileName, examClass[index].operateCount , examClass[index].limitTime);
+            }
+            else
+            {
+                ScoreManager.Instance.ReadExamData(index);
+            }
+
             SceneLoader.Instance.LoadScene(examClass[index].examSceneIndex);
             ScoreManager.Instance.StartCounting();
         }
---
+++ Assets/Resource/Global/Scripts/UI/WatchTime.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using TMPro;
+using UnityEngine;
+
+public class WatchTime : MonoBehaviour
+{
+    [SerializeField] private TaiwanCalendar taiwanTime;
+    [SerializeField] private string hour;
+    [SerializeField] private string minute;
+    [SerializeField] private int second;
+    [SerializeField] private TextMeshProUGUI timeTMPro;
+
+    void Start()
+    {
+        taiwanTime = new TaiwanCalendar();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // transform.localPosition = new Vector3(-0.55f, 0, 0.637f);
+        // transform.localRotation = Quaternion.identity;
+
+
+        if (taiwanTime.GetHour(DateTime.Now)<10)
+        {
+            hour = $"0{taiwanTime.GetHour(DateTime.Now)}";
+        }
+        else
+        {
+            hour = $"{taiwanTime.GetHour(DateTime.Now)}";
+        }
+
+        if (taiwanTime.GetMinute(DateTime.Now)<10)
+        {
+            minute = $"0{taiwanTime.GetMinute(DateTime.Now)}";
+        }
+        else
+        {
+            minute = $"{taiwanTime.GetMinute(DateTime.Now)}";
+        }
+
+        second = taiwanTime.GetSecond(DateTime.Now);
+        timeTMPro.text = $"{hour}\n{minute}";
+
+    }
+}
