cce9801dd6665ba67a12c9fadb4766811e290c80
manofmoutain <m97532007@gmail.com>
2021-06-22 17:02:37 +0800

    水流&水面上升

---
+++ Assets/Resource/Global/Scripts/UVFlow.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UVFlow : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private float timer;
+
+    [SerializeField] private Transform startPosition;
+    [SerializeField] private Transform endPosition;
+
+    private void OnEnable()
+    {
+        timer = 0;
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.Lerp(startPosition.position, endPosition.position, timer*0.25f);
+
+        timer += Time.deltaTime;
+        GetComponent<Renderer>().material.mainTextureOffset =
+            new Vector2(-speed * timer, speed * timer);
+    }
+}
