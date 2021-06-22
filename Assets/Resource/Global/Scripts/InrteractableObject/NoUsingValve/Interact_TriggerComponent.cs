08028b6d4a76a67e4c4fac2e294e5a7a2057d18d
manofmoutain <m97532007@gmail.com>
2021-06-22 14:31:49 +0800

    不重要的修改資訊

--- Assets/Resource/Global/Scripts/InrteractableObject/NoUsingValve/Interact_TriggerComponent.cs
+++ Assets/Resource/Global/Scripts/InrteractableObject/NoUsingValve/Interact_TriggerComponent.cs
@@ -1,12 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
 namespace InteractableObject
 {
     public class Interact_TriggerComponent : MonoBehaviour
     {
+        // private bool isEnter;
 
+        private void OnEnable()
+        {
+            // isEnter = false;
+        }
     }
 }
 
