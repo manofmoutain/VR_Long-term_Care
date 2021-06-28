84d9b0d3d8359b0beb6f9658c8159374e99956ab
manofmoutain <m97532007@gmail.com>
2021-06-28 14:01:55 +0800

    RCPManager-進入遊戲->取得pin碼->透過pin碼開啟FB登入->取得使用者資訊

--- Assets/Resource/Global/Scripts/Manager/TCPManager.cs
+++ Assets/Resource/Global/Scripts/Manager/TCPManager.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
+using Microsoft.CognitiveServices.Speech.Audio;
 using UnityEngine;
 using UnityEngine.Networking;
 
@@ -6,37 +8,138 @@ namespace Manager
 {
     public class TCPManager : Monosingleton<TCPManager>
     {
+        [Serializable]
+        public class DevicePin
+        {
+            public string pin;
+            public string device_id;
+            public string login_url;
+        }
+
+        [Serializable]
+        public class UserData
+        {
+            public string vr_session_id;
+            public string user_id;
+            public string user_name;
+        }
+
+        /// <summary>
+        /// 裝置pin
+        /// </summary>
+        [SerializeField] private DevicePin devicePin;
+
+        /// <summary>
+        /// 使用者資訊
+        /// </summary>
+        public UserData userData;
+
+        [SerializeField] private string getPinURI = "/api/v2/vr/session/";
+
+        /// <summary>
+        /// 教案ID
+        /// </summary>
+        [SerializeField] private string lesson_ID = "A99";
+
+        /// <summary>
+        /// 登入pollURL
+        /// </summary>
+        [SerializeField] private string devicePollingURL = "/api/v2/vr/session/poll/";
+
+        public bool isLogin;
+        public string UserID => userData.user_id;
+        public string UserName => userData.user_name;
+
         private void Start()
         {
+            devicePin = new DevicePin();
+            userData = new UserData();
+            isLogin = false;
 
+            StartCoroutine(Co_GetDevicePin());
         }
 
-        public void UploadData(string data)
+
+        /// <summary>
+        /// 獲取pin碼
+        /// </summary>
+        /// <param name="uri"></param>
+        /// <returns></returns>
+        IEnumerator Co_GetDevicePin()
         {
-            Application.OpenURL("https://nutcvr.rdto.io/");
-            StartCoroutine(Upload(data));
-        }
-
-        IEnumerator Upload(string data)
-        {
-            var myData = new byte[2048];
-            myData = System.Text.Encoding.UTF8.GetBytes(data);
-            UnityWebRequest unityWebRequest = new UnityWebRequest("https://nutcvr.rdto.io/api/v1/vr/session", "POST");
-
-            unityWebRequest.uploadHandler = (UploadHandler) new UploadHandlerRaw(myData);
-            unityWebRequest.downloadHandler = (DownloadHandler) new DownloadHandlerBuffer();
-            unityWebRequest.SetRequestHeader("AUTHORIZATION",
-                "rkgvmme5CBWMAfwpM9GOZRgP+2BgvknHv0Y3raH1mKzMP23nHzrRR4b6B!9Bgs0pdON@BAB!EPfXeCod");
-
-            yield return unityWebRequest.SendWebRequest();
-
-            if (unityWebRequest.result != UnityWebRequest.Result.Success)
+            string uri = $"https://nutcvr.rdto.io{getPinURI}{lesson_ID}";
+            using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
             {
-                print(unityWebRequest.error);
+                webRequest.SetRequestHeader("AUTHORIZATION",
+                    "rkgvmme5CBWMAfwpM9GOZRgP+2BgvknHv0Y3raH1mKzMP23nHzrRR4b6B!9Bgs0pdON@BAB!EPfXeCod");
+                webRequest.useHttpContinue = false;
+                webRequest.timeout = 60;
+                string[] pages = uri.Split('/');
+                int page = pages.Length - 1;
+                yield return webRequest.SendWebRequest();
+                switch (webRequest.result)
+                {
+                    case UnityWebRequest.Result.ConnectionError:
+                    case UnityWebRequest.Result.DataProcessingError:
+                        Debug.LogError(pages[page] + ": Error: " + webRequest.error);
+                        break;
+                    case UnityWebRequest.Result.ProtocolError:
+                        Debug.LogError(pages[page] + ": HTTP Error: " + webRequest.error);
+                        break;
+                    case UnityWebRequest.Result.Success:
+                        Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text);
+                        devicePin = JsonUtility.FromJson<DevicePin>(webRequest.downloadHandler.text);
+                        break;
+                }
+
+                yield return new WaitUntil(() => devicePin.pin != string.Empty);
+                Application.OpenURL(devicePin.login_url);
+                InvokeRepeating(nameof(LoginByPin), 1, 1);
             }
-            else
+        }
+
+
+        public void LoginByPin()
+        {
+            StartCoroutine(Co_Device_Login_State_Polling());
+        }
+
+
+        /// <summary>
+        /// 從pin碼取得使用者資訊
+        /// </summary>
+        /// <returns></returns>
+        IEnumerator Co_Device_Login_State_Polling()
+        {
+            string url = $"https://nutcvr.rdto.io{devicePollingURL}{devicePin.pin}";
+
+            using (UnityWebRequest webRequest = UnityWebRequest.Get(url))
             {
-                print("Upload complete");
+                webRequest.SetRequestHeader("AUTHORIZATION",
+                    "rkgvmme5CBWMAfwpM9GOZRgP+2BgvknHv0Y3raH1mKzMP23nHzrRR4b6B!9Bgs0pdON@BAB!EPfXeCod");
+
+                yield return webRequest.SendWebRequest();
+
+                switch (webRequest.result)
+                {
+                    case UnityWebRequest.Result.ConnectionError:
+                    case UnityWebRequest.Result.DataProcessingError:
+                        Debug.LogError("Error: " + webRequest.error);
+                        isLogin = false;
+                        break;
+                    case UnityWebRequest.Result.ProtocolError:
+                        Debug.LogError("HTTP Error: " + webRequest.error);
+                        isLogin = false;
+                        break;
+                    case UnityWebRequest.Result.Success:
+                        Debug.Log("Received: " + webRequest.downloadHandler.text);
+                        userData = JsonUtility.FromJson<UserData>(webRequest.downloadHandler.text);
+                        if (webRequest.downloadHandler.text != string.Empty)
+                        {
+                            isLogin = true;
+                        }
+                        break;
+                }
             }
         }
     }
