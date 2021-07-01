8c6edd2a1d250d014db6ce7886f1521720acb87b
manofmoutain <m97532007@gmail.com>
2021-07-01 15:39:06 +0800

    增加圍兜模型、材質圖，修改備餐配分方式

---
+++ Assets/Resource/04.Prepare Meal/Scripts/PatientEat.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Global.Pateint;
+using Manager;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace PrepareMeal
+{
+    public class PatientEat : MonoBehaviour
+    {
+        [SerializeField] private float timer;
+        [SerializeField] private bool isEating;
+        [SerializeField] private float eatingTime;
+
+        [Range(0,1)][SerializeField] private float hintFillValue;
+        [SerializeField] private Image eatingHint;
+
+
+        private void Update()
+        {
+            // eatingHint.fillAmount = hintFillValue;
+            if (isEating)
+            {
+                timer += Time.deltaTime;
+                hintFillValue = Mathf.Lerp(0,1 , timer*0.5f);
+                eatingHint.fillAmount = hintFillValue;
+                if (timer>=eatingTime)
+                {
+                    isEating = false;
+                }
+            }
+            else
+            {
+                timer = 0;
+                eatingHint.fillAmount = 0;
+            }
+        }
+
+        public void Eating(int index)
+        {
+            if (isEating && timer<=eatingTime)
+            {
+                //嗆到
+                GetComponent<Patient>().PlaySFX(0);
+                ScoreManager.Instance.IncreaseOperateSteps(index);
+            }
+            isEating = !isEating;
+
+
+        }
+    }
+}
+
---
+++ Assets/Resource/Global/Scripts/InrteractableObject/NoUsingValve/SwitchComponentEventCountOnUI.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using TMPro;
+
+namespace InteractableObject
+{
+    public class SwitchComponentEventCount : MonoBehaviour
+    {
+        [SerializeField] SwitchComponentEvent switchEvent;
+        [SerializeField] private TextMeshProUGUI countText;
+        [SerializeField] private string hint;
+
+        private void Start()
+        {
+            if (countText==null)
+            {
+                countText = GetComponentInChildren<TextMeshProUGUI>();
+            }
+        }
+
+        private void Update()
+        {
+            int count = switchEvent.triggerOBJs.Count + switchEvent.collisionOBJs.Count;
+            countText.text = $"{hint}:{count}";
+        }
+    }
+}
+
