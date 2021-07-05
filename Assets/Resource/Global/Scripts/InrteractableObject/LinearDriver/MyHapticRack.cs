b415d1c3689c9f4f78a3d60b93fbd368bd32e824
manofmoutain <m97532007@gmail.com>
2021-07-05 14:24:12 +0800

    線性驅動中的HapticRack改為不使用SteamVR的腳本

---
+++ Assets/Resource/Global/Scripts/InrteractableObject/LinearDriver/MyHapticRack.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Valve.VR.InteractionSystem;
+
+namespace InteractableObject
+{
+    [RequireComponent(typeof(MyInteractable))]
+    public class MyHapticRack : MonoBehaviour
+    {
+        [Tooltip( "The linear mapping driving the haptic rack" )]
+        		public Interact_LinearMapping linearMapping;
+
+        		[Tooltip( "The number of haptic pulses evenly distributed along the mapping" )]
+        		public int teethCount = 128;
+
+        		[Tooltip( "Minimum duration of the haptic pulse" )]
+        		public int minimumPulseDuration = 500;
+
+        		[Tooltip( "Maximum duration of the haptic pulse" )]
+        		public int maximumPulseDuration = 900;
+
+        		[Tooltip( "This event is triggered every time a haptic pulse is made" )]
+        		public UnityEvent onPulse;
+
+        		private Hand hand;
+        		private int previousToothIndex = -1;
+
+        		//-------------------------------------------------
+        		void Awake()
+        		{
+        			if ( linearMapping == null )
+        			{
+        				linearMapping = GetComponent<Interact_LinearMapping>();
+        			}
+        		}
+
+
+        		//-------------------------------------------------
+        		private void OnHandHoverBegin( Hand hand )
+        		{
+        			this.hand = hand;
+        		}
+
+
+        		//-------------------------------------------------
+        		private void OnHandHoverEnd( Hand hand )
+        		{
+        			this.hand = null;
+        		}
+
+
+        		//-------------------------------------------------
+        		void Update()
+        		{
+        			int currentToothIndex = Mathf.RoundToInt( linearMapping.value * teethCount - 0.5f );
+        			if ( currentToothIndex != previousToothIndex )
+        			{
+        				Pulse();
+        				previousToothIndex = currentToothIndex;
+        			}
+        		}
+
+
+        		//-------------------------------------------------
+        		private void Pulse()
+        		{
+        			if ( hand && (hand.isActive) && ( hand.GetBestGrabbingType() != GrabTypes.None ) )
+        			{
+        				ushort duration = (ushort)Random.Range( minimumPulseDuration, maximumPulseDuration + 1 );
+        				hand.TriggerHapticPulse( duration );
+
+        				onPulse.Invoke();
+        			}
+        		}
+    }
+}
+
