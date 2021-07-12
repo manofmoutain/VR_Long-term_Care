4dd51592d8c31f908d5009121ec8a037881d13f7
manofmoutain <m97532007@gmail.com>
2021-07-12 16:56:05 +0800

    增加備餐專用腳本-將水灌入針筒、灌入藥水

---
+++ Assets/Resource/04.Prepare Meal/Scripts/InjectWater.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace PrepareMeal
+{
+    public class InjectWater : MonoBehaviour
+    {
+        [SerializeField] private UnityEvent waterInjectEvent;
+        private void OnTriggerStay(Collider other)
+        {
+            if (other.name=="裝在量杯裡面的水")
+            {
+                waterInjectEvent.Invoke();
+            }
+        }
+    }
+}
+
---
+++ Assets/Resource/04.Prepare Meal/Scripts/PotionSink.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+namespace PrepareMeal
+{
+    public class PotionSink : MonoBehaviour
+    {
+        public bool isInject;
+
+        [SerializeField] private float moveSpeed;
+        private float timer;
+
+
+        /// <summary>
+        /// 針筒
+        /// </summary>
+        [SerializeField] private GameObject Syringe;
+
+        [SerializeField] private GameObject plug;
+        [SerializeField] private float start;
+        [SerializeField] private float end;
+
+        private void OnEnable()
+        {
+            transform.localScale = Vector3.one;
+            timer = 0;
+        }
+
+        private void Update()
+        {
+            if (Syringe.transform.position.y>plug.transform.position.y && isInject)
+            {
+                timer += Time.deltaTime;
+                transform.localScale = new Vector3(1,Mathf.Lerp(start,end,timer*moveSpeed),1);
+                if (transform.localScale.y==end)
+                {
+                    isInject = false;
+                }
+            }
+        }
+
+
+        public void InputWater()
+        {
+            timer += Time.deltaTime;
+            transform.localScale = new Vector3(1,Mathf.Lerp(0,1,timer*moveSpeed),1);
+            start = transform.localScale.y;
+        }
+    }
+}
+
